215076678d6cb97fd9f90e41467b727daf1e0a5f
ikura1 <ikura.lab@gmail.com>
2021-07-10 00:01:31 +0900

    feat: ReactロゴとNfcButtonを置換

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { IconButton } from '@material-ui/core';
 import NfcIcon from '@material-ui/icons/Nfc';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <IconButton style={{ backgroundColor: "#4c5361" }}>
           {/* Clicked NFC Reader */}
           {/* Loading Popup */}
           {/* Loaded Result Popup */}
-          <NfcIcon style={{ color: "white", fontSize: 40}}/>
+          <NfcIcon style={{ color: "white", fontSize: 200}}/>
         </IconButton>
         {/* NFC Result Status */}
       </header>
