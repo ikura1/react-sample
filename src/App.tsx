3307d21542574b7a412a078ba44deb5a739cbe3c
ikura1 <ikura.lab@gmail.com>
2021-07-10 14:19:45 +0900

    feat: functionからclassに変更 - CircularProgressを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { IconButton } from '@material-ui/core';
+import { IconButton, CircularProgress } from '@material-ui/core';
 import NfcIcon from '@material-ui/icons/Nfc';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <IconButton style={{ backgroundColor: "#4c5361" }}>
-          {/* Clicked NFC Reader */}
-          {/* Loading Popup */}
-          {/* Loaded Result Popup */}
-          <NfcIcon style={{ color: "white", fontSize: 200}}/>
-        </IconButton>
-        {/* NFC Result Status */}
-      </header>
-    </div>
-  );
+interface AppState {
+  reading: boolean;
 }
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {reading: false};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <IconButton style={{ display: !this.state.reading ? "block": "none", backgroundColor: "#4c5361" }} onClick={() => this.readNfc() }>
+            {/* Clicked NFC Reader */}
+            {/* Loading Popup (Replace or (hide and show))*/}
+            {/* Loaded Result Popup */}
+            <NfcIcon style={{ color: "white", fontSize: 200}}/>
+          </IconButton>
+          <CircularProgress size={200} style={{ display: this.state.reading ? "block" : "none" }}/>
+          {/* NFC Result Status */}
+        </header>
+      </div>
+    );
+  }
+
+  async readNfc() {
+    this.setState({reading: true});
+  }
+}
+
 
 export default App;
