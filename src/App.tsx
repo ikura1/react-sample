5d7529b280020ca74b75d0ecea208b123abdfb2d
ikura1 <ikura.lab@gmail.com>
2021-07-13 19:19:12 +0900

    feat: NFCの読み込み関数を追加 - NFCに対応してない分岐を関数の先頭に移動 - handleOpen, handleCloseをreadNfc, cancelNfcに変更

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Alert from "@material-ui/lab/Alert"
 import CircularProgress from "@material-ui/core/CircularProgress"
 import IconButton from "@material-ui/core/IconButton"
 import NfcIcon from "@material-ui/icons/Nfc"
+import ErrorIcon from "@material-ui/icons/Error"
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles"
 
 import "./App.css"
@@ -19,37 +20,53 @@ const useStyles = makeStyles((theme: Theme) =>
 function App() {
   const classes = useStyles()
   const [open, setOpen] = React.useState(false)
-  const handleClose = () => {
+
+  if (!("NDEFReader" in window)) {
+    return (
+      <div className="App">
+        <Alert severity="error" style={{ display: "flex" }}>
+          WebNFC is not Supported
+        </Alert>
+        <header className="App-header">
+          <ErrorIcon style={{ color: "white", fontSize: 200 }} />
+        </header>
+      </div>
+    )
+  }
+  const ndef = new NDEFReader()
+  let controller: any = null
+
+  const cancelNfc = () => {
+    if (controller) {
+      controller.abort()
+      controller = null
+    }
     setOpen(false)
   }
 
-  const handleToggle = () => {
+  const readNfc = async () => {
     setOpen(!open)
+    controller = new AbortController()
+    await ndef.scan({ signal: controller.signal })
+    ndef.onreading = ({ message }) => {
+      alert(`Message read from a NFC tag: ${message}`)
+      cancelNfc()
+    }
   }
   return (
     <div className="App">
-      <Alert
-        severity="error"
-        style={{ display: !("NDEFReader" in window) ? "flex" : "none" }}
-      >
-        WebNFC is not Supported
-      </Alert>
       <header className="App-header">
         {/* Clicked NFC Reader */}
         <IconButton
           style={{
             backgroundColor: "#4c5361",
           }}
-          onClick={handleToggle}
+          onClick={readNfc}
         >
           <NfcIcon style={{ color: "white", fontSize: 200 }} />
         </IconButton>
         {/* Loading Popup (Replace or (hide and show))*/}
-        <Backdrop
-          className={classes.backdrop}
-          open={open}
-          onClick={handleClose}
-        >
+        <Backdrop className={classes.backdrop} open={open} onClick={cancelNfc}>
           <CircularProgress size={100} color="inherit" />
         </Backdrop>
         {/* Loaded Result Popup */}
